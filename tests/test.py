eaf736aedc8fa7b70e9057b172d0d00f7b6428a5
drill_n_bass <77461241+Drill-N-Bass@users.noreply.github.com>
2022-01-28 06:13:56 +0100

    Adding new tests for Unittest execution. Testing classes with faulty scripts.

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,6 @@
 import unittest
+from fractions import Fraction
+
 
 from my_sum import sum
 
@@ -6,11 +8,44 @@ from my_sum import sum
 class TestSum(unittest.TestCase):
     def test_list_int(self):
         """
-        Test that it can sum a list of integers
+        Test that it can sum a list of integers: 
         """
         data = [1, 2, 3]
         result = sum(data)
         self.assertEqual(result, 6)
+
+
+
+class TestSumNot6(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers - here the list != 6:
+        """
+        data = [1, 2, 10]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+class TestSumStringList(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers - here the list != 6 and one of list's object is a string:
+        """
+        data = [1, 2, "Cat"]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+class TestSumFraction(unittest.TestCase):
+    def test_list_fraction(self):
+        """
+        Test that it can sum a list of fractions
+        """
+        data = [Fraction(1, 4), Fraction(1, 4), Fraction(2, 5)]
+        result = sum(data)
+        self.assertEqual(result, 1)
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
